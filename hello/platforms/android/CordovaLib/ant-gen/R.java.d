638f12077a04bd18f91db5f6d9dd85d706b33a50
Tianyu Shi <ts133@duke.edu>
2014-09-29 19:11:19 -0700

    Create xcode project

---
+++ hello/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/tianyushi/Desktop/hello/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ hello/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/tianyushi/Desktop/hello/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/tianyushi/Desktop/hello/platforms/android/AndroidManifest.xml \
