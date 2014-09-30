822442ff3f7b2be0ef3d871e868bb25099b199a9
vjkaruna <vjkaruna@gmail.com>
2014-09-29 19:41:58 -0700

    Sign in, sign up.

--- challenge/ViewController.swift
+++ challenge/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInAction(sender: AnyObject) {
+        var username = emailTextField.text
+        var password = passwordTextField.text
+        PFUser.logInWithUsernameInBackground(username, password:password) {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                println("user \(user.username)")
+            } else {
+               println("Error 1")
+            }
+        }
+    }
 
+    @IBAction func signUpAction(sender: AnyObject) {
+        var pfuser = PFUser()
+        
+        pfuser.username = emailTextField.text
+        pfuser.password = passwordTextField.text
+        pfuser.email = emailTextField.text
+        
+        pfuser.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, error: NSError!) -> Void in
+            if error == nil {
+                println("user \(pfuser.username)")
+            } else {
+                let errorString = (error.userInfo! as NSDictionary)["error"] as NSString
+                // Show the errorString somewhere and let the user try again.
+                println("Error 2 \(errorString)")
+            }
+        }
+    }
 }
 
